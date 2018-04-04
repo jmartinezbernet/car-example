05fadae1ec16e6486a76334869e8aa7ed263adf7
Javier Martinez Bernet <jmartinezbernet@gmail.com>
2018-04-04 08:24:40 +0200

    Added zend paginator

---
+++ Car/src/Infrastructure/Di/ServiceManager/Factories/FindCarsByCriteriaQueryFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmartinez
+ * Date: 4/04/18
+ * Time: 8:16
+ */
+
+namespace Car\Infrastructure\Di\ServiceManager\Factories;
+
+
+class FindCarsByCriteriaQueryFactory
+{
+
+}
---
+++ Car/src/Infrastructure/Query/MySql/MySqlFindCarsByCriteriaQuery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmartinez
+ * Date: 4/04/18
+ * Time: 8:17
+ */
+
+namespace Car\Infrastructure\Query\MySql;
+
+
+class MySqlFindCarsByCriteriaQuery
+{
+
+}
