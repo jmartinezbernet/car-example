7a093a200b83c114ac19cd4dd521869ec15d6c24
Javier Martinez Bernet <jmartinezbernet@gmail.com>
2018-04-04 08:38:31 +0200

    Fixed CarData sql query

--- Car/src/Infrastructure/Ui/Console/PhpMig/Sql/CarData.sql
+++ Car/src/Infrastructure/Ui/Console/PhpMig/Sql/CarData.sql
@@ -1,5 +1,5 @@
 INSERT INTO car (id, data) values
-('1', '{"id":"1abdc-hasjd-k3jkasd", "brand":"Audi", "model":"A3"}'),
-('1', '{"id":"1abdc-hasjd-k3jkasd", "brand":"Audi", "model":"A4"}'),
-('1', '{"id":"1abdc-hasjd-k3jkasd", "brand":"Audi", "model":"A5"}'),
-('1', '{"id":"1abdc-hasjd-k3jkasd", "brand":"Renault", "model":"Megane"}');
+('1abdc-hasjd-k3jkasd', '{"id":"1abdc-hasjd-k3jkasd", "brand":"Audi", "model":"A3"}'),
+('2abdc-asdff-k3jkasd', '{"id":"2abdc-asdff-k3jkasd", "brand":"Audi", "model":"A4"}'),
+('3abdc-6456h-k3jkasd', '{"id":"3abdc-6456h-k3jkasd", "brand":"Audi", "model":"A5"}'),
+('4abdc-435fd-k3jkasd', '{"id":"4abdc-435fd-k3jkasd", "brand":"Renault", "model":"Megane"}');
